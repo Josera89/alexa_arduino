046a732a80c32ba878d75898493a137bc0644af5
Jose Zuniga <jramon.zu@gmail.com>
2020-05-19 12:10:57 -0500

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (res, req) {
   res.send('Hello world(test2)!\n');
 })
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Listening on port', port);
